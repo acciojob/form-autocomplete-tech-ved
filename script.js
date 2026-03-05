400a96acb0b7b9ddf1504f7044d1bacfe38e263f
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-03-05 09:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+// load saved data
+window.addEventListener("load", () => {
+  const savedData = JSON.parse(localStorage.getItem("formData"));
+
+  if (savedData) {
+    inputs.forEach(input => {
+      if (savedData[input.name]) {
+        input.value = savedData[input.name];
+      }
+    });
+  }
+});
+
+// save data
+form.addEventListener("input", () => {
+  let data = {};
+
+  inputs.forEach(input => {
+    data[input.name] = input.value;
+  });
+
+  localStorage.setItem("formData", JSON.stringify(data));
+});
